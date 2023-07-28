a6a599ccbfddffb30c303e4bbb3c37484ef62983
ermar <ermar@DESKTOP-0CSLR0O.attlocal.net>
2023-07-27 22:23:42 -0400

    create first class

---
+++ src/demo/repository3/DemoRepoTest3.java
@@ -0,0 +1,9 @@
+package demo.repository3;
+
+public class DemoRepoTest3 {
+	
+	public static void main(String[] args) {
+		System.out.println("This is a demo to create a repository");
+		System.out.println("demo");
+	}
+}
